f58517d0dc7bafe1ce27d04f0865fc63e6637d9d
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-06-25 15:59:40 +0200

    some refactoring

--- flake.nix
+++ flake.nix
@@ -8,19 +8,11 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs: 
+  outputs = { self, nixpkgs, ... }:
     let 
       eachSystem = nixpkgs.lib.genAttrs [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
-    in rec {
-      overlays.default = (final: prev: {
-        nixln-edit = inputs.nixln-edit.packages.${final.system}.nixln-edit;
-
-        vimPlugins = (prev.vimPlugins or {}) // {
-          battery-nvim = prev.callPackage ./packages/vimPlugins/battery-nvim.nix { };
-          isabelle-syn-nvim = prev.callPackage ./packages/vimPlugins/isabelle-syn-nvim.nix { };
-          telescope-tabs = prev.callPackage ./packages/vimPlugins/telescope-tabs.nix { };
-        };
-      });
-      packages = eachSystem (system: nixpkgs.legacyPackages.${system}.extend (overlays.default));
+    in {
+      overlays.default = import ./. { inherit self; };
+      packages = eachSystem (system: import ./packages.nix { pkgs = import nixpkgs { inherit system; }; inherit self; });
     };
 }
