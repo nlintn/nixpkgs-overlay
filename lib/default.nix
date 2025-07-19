c068ef15e8770c464bfcdecc8fec5e2d8998e74d
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-07-19 17:11:01 +0200

    add eachSystem + eachSystemPkgs to lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,12 +1,14 @@
 lib:
 
 let
-  main = {
+  main = rec {
     capitalizeString = str: (
       (lib.toUpper (lib.substring 0 1 str))
       +
       (lib.substring 1 ((lib.stringLength str) - 1) str)
     );
+    eachSystem = nixpkgs: lib.genAttrs (builtins.attrNames nixpkgs.legacyPackages);
+    eachSystemPkgs = nixpkgs: f: builtins.mapAttrs (_: f) (eachSystem nixpkgs (system: import nixpkgs { inherit system; }));
   };
   lib' = lib // main;
 in main // {
