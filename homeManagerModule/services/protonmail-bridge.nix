ac0b063486d63f28ba704b328811b83316c2e16c
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-08-21 00:21:34 +0200

    strip protonmail-bridge module because now in hm base

--- homeManagerModule/services/protonmail-bridge.nix
+++ homeManagerModule/services/protonmail-bridge.nix
@@ -4,22 +4,7 @@ let
   cfg = config.services.protonmail-bridge;
 in {
   options.services.protonmail-bridge = {
-    enable = lib.mkEnableOption "ProtonMail Bridge";
-    package = lib.mkPackageOption pkgs "protonmail-bridge" { };
-    logLevel = lib.mkOption {
-      default = null;
-      description = "Log level of the ProtonMail Bridge service. If set to null then the service uses it's default log level.";
-      type = lib.types.nullOr (
-        lib.types.enum [
-          "panic"
-          "fatal"
-          "error"
-          "warn"
-          "info"
-          "debug"
-        ]
-      );
-    };
+    enableCfgScript = lib.mkEnableOption "ProtonMail Bridge config script";
   };
 
   config = let
@@ -30,28 +15,13 @@ in {
       }
       builtin trap -- "restart-bridge;exit" \
           SIGABRT SIGALRM SIGBUS SIGFPE SIGHUP SIGILL SIGINT SIGIO \
-          SIGPIPE SIGPOLL SIGPROF SIGPWR SIGQUIT SIGSEGV SIGSYS \
+          SIGPIPE SIGIO SIGPROF SIGPWR SIGQUIT SIGSEGV SIGSYS \
           SIGTERM SIGTRAP SIGUSR1 SIGUSR2 SIGVTALRM SIGXCPU SIGXFSZ &&
       ${config.systemd.user.systemctlPath} --user stop ${unitName}.service &&
       ${cfg.package}/bin/protonmail-bridge --cli;
       restart-bridge
     '';
-  in lib.mkIf cfg.enable {
+  in lib.mkIf (cfg.enable && cfg.enableCfgScript) {
     home.packages = [ cfgScript ];
-
-    systemd.user.services.${unitName} = {
-      Unit = {
-        After = [ "graphical-session.target" ];
-        Description = "ProtonMail Bridge";
-      };
-      Install.WantedBy = [ "graphical-session.target" ];
-
-      Service = let
-        logLevel = lib.optionalString (cfg.logLevel != null) "--log-level ${cfg.logLevel}";
-      in {
-        ExecStart = "${cfg.package}/bin/protonmail-bridge -n ${logLevel}";
-        Restart = "always";
-      };
-    };
   };
 }
