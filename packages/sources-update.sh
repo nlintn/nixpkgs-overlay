1fa0cc561c3e9e47c429c72ff2a54d643d15e404
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-08-25 18:36:30 +0200

    improve update script

--- packages/sources-update.sh
+++ packages/sources-update.sh
@@ -1,18 +1,25 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eu -o pipefail
 builtin cd -- $(dirname $0)
-echo " + $(pwd)/$(basename $0)"
+echo -e " + $(pwd)/$(basename $0)\n"
 
-export NIX_PATH="nixpkgs=flake:$(nix-instantiate --eval --expr --raw '(builtins.getFlake (builtins.toString ./..)).inputs.nixpkgs.outPath')"
+export NIXPKGS="$(nix-instantiate --eval --expr --raw '(builtins.getFlake (builtins.toString ./..)).inputs.nixpkgs.outPath')"
+export NIX_PATH="nixpkgs=flake:$NIXPKGS"
 
-json=$(nix-build --no-out-link --expr \
-    'let pkgs = import <nixpkgs> {}; in (pkgs.formats.json {}).generate "sources" (
+prev_json=$(cat sources.json)
+json=$(nix-instantiate --option substitute false --eval --raw --argstr nixpkgs "$NIXPKGS" --expr '
+    { nixpkgs }:
+    let
+        pkgs = import nixpkgs {};
+    in builtins.toJSON (
         builtins.mapAttrs (url: {type ? "git", ...} @ args:
             (attrs: args // (pkgs.lib.filterAttrs (n: _: n == "narHash" || n == "rev" || n == "submodules" ) attrs))
             (builtins.fetchTree { inherit type url; }))
-        (builtins.fromJSON (builtins.readFile ./sources.json)))' \
-    ) &&
-cat $json > sources.json
-echo "updated $(pwd)/sources.json"
+        (builtins.fromJSON (builtins.readFile ./sources.json)))'
+)
+echo $json | jq --sort-keys . > sources.json
+echo -e "updated $(pwd)/sources.json"
+diff --color --ignore-all-space --initial-tab --unified=1 <(echo "$prev_json" | jq --sort-keys .) sources.json || true
+echo
 
 for f in $(find -regex './.*/sources-update\.sh'); do "$f"; done
