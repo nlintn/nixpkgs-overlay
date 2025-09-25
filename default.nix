f90d0d3349a806bbc8c833d0784a8e48ea716f68
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-09-25 16:51:36 +0200

    use .extend recursively in overlay instead of lib.recursiveUpdate

--- default.nix
+++ default.nix
@@ -1,1 +1,4 @@
-final: prev: prev.lib.recursiveUpdate prev (import ./packages { pkgs = prev; })
+let
+  recursiveExtend = base: override:
+    builtins.mapAttrs (n: v: if base ? ${n} && base.${n} ? extend then base.${n}.extend (final: prev: recursiveExtend prev v) else v) override;
+in final: prev: recursiveExtend prev (import ./packages { pkgs = prev; })
