bffab56dc8e6cd37b5fb7e8813dd2c5a9e5e448a
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2025-11-27 00:28:11 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/ee09932cedcef15aaf476f9343d1dea2cb77e261?narHash=sha256-9glbI7f1uU%2ByzQCq5LwLgdZqx6svOhZWkd4JRY265fc%3D' (2025-11-23)
      → 'github:NixOS/nixpkgs/bb813de6d2241bcb1b5af2d3059f560c66329967?narHash=sha256-2bCmfCUZyi2yj9FFXYKwsDiaZmizN75cLhI/eWmf3tk%3D' (2025-11-26)

--- check-build.nix
+++ check-build.nix
@@ -2,5 +2,5 @@ pkgs: self:
 
 pkgs.symlinkJoin {
   name = "check-build";
-  paths = builtins.filter pkgs.lib.isDerivation (self.lib.attrsToListRecursive self.legacyPackages.${pkgs.system});
+  paths = builtins.filter pkgs.lib.isDerivation (self.lib.attrsToListRecursive self.legacyPackages.${pkgs.stdenv.hostPlatform.system});
 }
