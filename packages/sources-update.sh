20aa572df388cf7dd73f5cfbfad6c88410094f7f
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2026-01-04 22:36:36 +0100

    sources-update.sh: allow to specify ref

--- packages/sources-update.sh
+++ packages/sources-update.sh
@@ -12,10 +12,14 @@ json=$(nix-instantiate --option substitute false --eval --raw --argstr nixpkgs "
     { nixpkgs }:
     let
         pkgs = import nixpkgs {};
+        lib = pkgs.lib;
+        filterTreeArgs = type: lib.filterAttrs (n: _: builtins.elem n {
+          git = [ "url" "ref" "submodules" ];
+        }.${type});
     in builtins.toJSON (
         builtins.mapAttrs (url: { type ? "git", ... } @ args:
-            (attrs: { inherit type; } // args // (pkgs.lib.filterAttrs (n: _: n == "narHash" || n == "rev" || n == "submodules" ) attrs))
-            (builtins.fetchTree { inherit type url; }))
+            (attrs: { inherit type; } // args // (lib.filterAttrs (n: _: n == "narHash" || n == "rev") attrs))
+            (builtins.fetchTree (filterTreeArgs type args // { inherit type url; })))
         (builtins.fromJSON (builtins.readFile ./sources.json)))'
 )
 echo "$json" | jq --sort-keys . > sources.json
