565f485f866054f740dec31a6a42b1478b7e5b27
Nico Lintner <118087966+nlintn@users.noreply.github.com>
2026-01-20 20:05:53 +0100

    chore(nix): update `vicinaeExtensions` build code

--- packages/by-path/vicinaeExtensions/package.nix
+++ packages/by-path/vicinaeExtensions/package.nix
@@ -1,8 +1,11 @@
+# code from https://github.com/vicinaehq/extensions/blob/90c5e6f208bca1b08543a1a09d76b60ddd30d0fe/flake.nix
+
 fetchSources: _:
 
 {
   lib,
   mkVicinaeExtension,
+  stdenv,
 }:
 
 let
@@ -13,8 +16,19 @@ lib.pipe (builtins.readDir "${extensions}/extensions") [
   (lib.mapAttrs (
     name: _type:
     mkVicinaeExtension {
-      pname = name;
-      src = "${extensions}/extensions/${name}";
+      inherit name;
+      version = "unstable";
+      src = stdenv.mkDerivation {
+        name = "${name}-patched-tsconfig";
+        src = "${extensions}/extensions/${name}";
+        buildPhase = ''
+          substituteInPlace tsconfig.json --replace "../../" "${extensions}/"
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r . $out
+        '';
+      };
     }
   ))
   (lib.flip lib.removeAttrs [
