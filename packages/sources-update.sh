a49f4ba5d89cf735091e368d9d6e746d2a015171
Nico Lintner <git@nlintn.de>
2026-03-09 04:41:35 +0100

    fix(sources): don't nuke lockfile if update fails

--- packages/sources-update.sh
+++ packages/sources-update.sh
@@ -10,12 +10,13 @@ NIXPKGS="$(nix-instantiate --eval --raw --argstr basePath "${BASE_PATH}" \
     --expr '{ basePath }: (builtins.getFlake (builtins.toString ./${basePath}/..)).inputs.nixpkgs.outPath')"
 NIX_PATH="nixpkgs=flake:${NIXPKGS}"
 
-prev_json="$(< "${BASE_PATH}/sources.json")"
-nix-instantiate --option substitute false --eval --raw --argstr json "${prev_json}" --argstr nixpkgs "$NIXPKGS" "${BASE_PATH}/sources-update.nix" \
-    | jq --sort-keys . > "${BASE_PATH}/sources.json"
+prev_json="$(<"${BASE_PATH}/sources.json")"
+new_json=$(nix-instantiate --option substitute false --eval --raw --argstr json "${prev_json}" --argstr nixpkgs "$NIXPKGS" "${BASE_PATH}/sources-update.nix" |
+    jq --sort-keys .)
+echo "${new_json}" >"${BASE_PATH}/sources.json"
 
 echo -e "updated ${BASE_PATH}/sources.json"
-diff --color --ignore-all-space --initial-tab --unified=1 <(jq --sort-keys . <<< "${prev_json}") "${BASE_PATH}/sources.json" || :
+diff --color --ignore-all-space --initial-tab --unified=1 <(jq --sort-keys . <<<"${prev_json}") "${BASE_PATH}/sources.json" || :
 echo
 
 find "${BASE_PATH}" -regex "${BASE_PATH}/.*/sources-update\.sh" -exec '{}' ';'
