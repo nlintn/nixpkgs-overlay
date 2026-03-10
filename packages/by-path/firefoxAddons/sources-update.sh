9d6b310d4a6d959437093435c70c8ed0cdbae2c7
Nico Lintner <git@nlintn.de>
2026-03-10 03:00:54 +0100

    feat(sources): override nixpkgs input of `mozilla-addons-to-nix`

--- packages/by-path/firefoxAddons/sources-update.sh
+++ packages/by-path/firefoxAddons/sources-update.sh
@@ -4,9 +4,12 @@ set -eu -o pipefail
 BASE_PATH="$(dirname "${0}")"
 echo -e " + ${0}\n"
 
-prev_addons="$(< "${BASE_PATH}/addons-generated.nix")"
-nix run --option experimental-features "flakes nix-command" "git+https://git.sr.ht/~rycee/mozilla-addons-to-nix/" \
-    "${BASE_PATH}/addons.json" "${BASE_PATH}/addons-generated.nix"
+prev_addons="$(<"${BASE_PATH}/addons-generated.nix")"
+nix run \
+  --option experimental-features "flakes nix-command" \
+  --override-input nixpkgs "${NIXPKGS}" \
+  "git+https://git.sr.ht/~rycee/mozilla-addons-to-nix/" -- \
+  "${BASE_PATH}/addons.json" "${BASE_PATH}/addons-generated.nix"
 
 diff --color --ignore-all-space --initial-tab --unified=2 <(echo "$prev_addons") "${BASE_PATH}/addons-generated.nix" || :
 echo
